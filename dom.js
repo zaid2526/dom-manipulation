0efa855d9f5a36fe0f0fc97d3f772e0de1ad41e8
zaid2526 <mzr16894@.com>
2022-05-11 14:46:55 +0530

    task 3 completed

--- dom.js
+++ dom.js
@@ -1,5 +1,5 @@
 // examin the documents object
-
+/*
 console.log(document);
 console.log(document.all);
 console.log(document.doctype);
@@ -11,3 +11,30 @@ console.log(document.all[10]);
 
 document.all[10].textContent=123;//this is not the way..
 // if we add any element, index of this element may be changes.
+
+*/
+
+//getElemenById
+console.log(document.getElementById('header-title'));
+let headerTitle=document.getElementById('header-title');
+console.log(headerTitle);
+console.log(headerTitle.value);
+headerTitle.textContent='hello';
+headerTitle.innerText='new action'
+// the only diffrence bw these is....
+// textContent does not take care of style attribute of element
+// while innerText take care of style attribute of element
+
+headerTitle.innerHTML="<h3>hello</h3>"
+//innerHTML does not change existing element,
+//it puuting the content inside  the element
+//example... <h1 id="header-title"> <h3>hello</h3></h1>
+
+const mainHeader=document.getElementById('main-header');
+mainHeader.style.borderBottom='solid 4px black';
+
+//const addItem=document.getElementsByClassName('title');
+const addItem=document.querySelector('.title');
+console.log(addItem);
+addItem.innerHTML='<h1> Add Item</h1>';
+addItem.style.color='green'
