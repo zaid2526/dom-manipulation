d8a1ad99c03e9ee83bd2f8e46f370739ab3c916d
zaid2526 <mzr16894@.com>
2022-05-14 01:08:57 +0530

    booking appointment app (task10) completed

---
+++ bookingApplication/bookingApp.js
@@ -0,0 +1,23 @@
+var button=document.getElementById('btn');
+
+button.addEventListener('click',runEvent);
+
+function runEvent(e){
+    console.log(e.target);
+    var name=document.getElementById('name')
+    var email=document.getElementById('email')
+    var phone=document.getElementById('phone')
+    //var user=document.querySelector('#my-form').getElementsByTagName('input[type=text]');
+    //console.log(user);
+    console.log(document.getElementById('name').value);
+    localStorage.setItem(`name${name.value}`,`${name.value}`)
+    localStorage.setItem(`email${email.value}`,`${email.value}`)
+    localStorage.setItem(`phone${phone.value}`,`${phone.value}`)
+
+
+    //clear input field..
+    name.value='';
+    email.value='';
+    phone.value='';
+    
+}
